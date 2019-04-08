a6da5b657607e02f25c36e4843db1cb27176bdb2
dudoser <frielender@gmail.com>
2019-04-08 19:26:24 +0700

    тупа коммит

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-def post_list(request):
+
+def base(request ):
+    return render(request, 'blog/post_list.html', {})
+
+def post_list(request, pk ):
+    print(pk)
     return render(request, 'blog/post_list.html', {})
